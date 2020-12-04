b06416d4039dab34e88c8c03e7d097a6b9296c4a
Katie Byers <lobsterkatie@gmail.com>
2020-12-03 18:17:17 -0800

    split transactionToSentryRequest into its own method, use transaction's tracestate value in envelope

--- packages/core/src/request.ts
+++ packages/core/src/request.ts
@@ -1,8 +1,92 @@
 import { Event, SentryRequest, Session } from '@sentry/types';
+import { base64ToUnicode, logger } from '@sentry/utils';
 
 import { API } from './api';
 
-/** Creates a SentryRequest from an event. */
+/**
+ * Create a SentryRequest from an error, message, or transaction event.
+ *
+ * @param event The event to send
+ * @param api Helper to provide the correct url for the request
+ * @returns SentryRequest representing the event
+ */
+export function eventToSentryRequest(event: Event, api: API): SentryRequest {
+  if (event.type === 'transaction') {
+    return transactionToSentryRequest(event, api);
+  }
+  return {
+    body: JSON.stringify(event),
+    type: event.type || 'event',
+    url: api.getStoreEndpointWithUrlEncodedAuth(),
+  };
+}
+
+/**
+ * Create a SentryRequest from a transaction event.
+ *
+ * Since we don't need to manipulate envelopes nor store them, there is no exported concept of an Envelope with
+ * operations including serialization and deserialization. Instead, we only implement a minimal subset of the spec to
+ * serialize events inline here. See https://develop.sentry.dev/sdk/envelopes/.
+ *
+ * @param event The transaction event to send
+ * @param api Helper to provide the correct url for the request
+ * @returns SentryRequest in envelope form
+ */
+export function transactionToSentryRequest(event: Event, api: API): SentryRequest {
+  // because we send event.tracestate as a header whose values have meaningful equals signs, when we create it we have
+  // to replace the padding character with something else; the `.replace` call here reverses that to make it valid
+  // base64 before converting to unicode for parsing
+  let tracestateJSON;
+  try {
+    tracestateJSON = event.tracestate ? base64ToUnicode(event.tracestate.replace('.', '=')) : undefined;
+  } catch (err) {
+    logger.warn(err);
+    tracestateJSON = '';
+  }
+  delete event.tracestate;
+
+  const envelopeHeaders = JSON.stringify({
+    event_id: event.event_id,
+    sent_at: new Date().toISOString(),
+    trace_id: event.contexts?.trace?.trace_id,
+    trace: tracestateJSON, // trace context for dynamic sampling on relay
+  });
+
+  const itemHeaders = JSON.stringify({
+    type: event.type,
+    // The content-type is assumed to be 'application/json' and not part of
+    // the current spec for transaction items, so we don't bloat the request
+    // body with it.
+    //
+    // content_type: 'application/json',
+    //
+    // The length is optional. It must be the number of bytes in req.Body
+    // encoded as UTF-8. Since the server can figure this out and would
+    // otherwise refuse events that report the length incorrectly, we decided
+    // not to send the length to avoid problems related to reporting the wrong
+    // size and to reduce request body size.
+    //
+    // length: new TextEncoder().encode(req.body).length,
+  });
+
+  const req: SentryRequest = {
+    // The trailing newline is optional; leave it off to avoid sending unnecessary bytes.
+    // body: `${envelopeHeaders}\n${itemHeaders}\n${JSON.stringify(event)\n}`,
+    body: `${envelopeHeaders}\n${itemHeaders}\n${JSON.stringify(event)}`,
+    type: 'transaction',
+    url: api.getEnvelopeEndpointWithUrlEncodedAuth(),
+  };
+
+  return req;
+}
+
+/**
+ * Create a SentryRequest from a session event.
+ *
+ * @param event The session event to send
+ * @param api Helper to provide the correct url for the request
+ * @returns SentryRequest in envelope form
+ */
 export function sessionToSentryRequest(session: Session, api: API): SentryRequest {
   const envelopeHeaders = JSON.stringify({
     sent_at: new Date().toISOString(),
@@ -16,63 +100,4 @@ export function sessionToSentryRequest(session: Session, api: API): SentryReques
     type: 'session',
     url: api.getEnvelopeEndpointWithUrlEncodedAuth(),
   };
-}
-
-/** Creates a SentryRequest from an event. */
-export function eventToSentryRequest(event: Event, api: API): SentryRequest {
-  const useEnvelope = event.type === 'transaction';
-
-  const req: SentryRequest = {
-    body: JSON.stringify(event),
-    type: event.type || 'event',
-    url: useEnvelope ? api.getEnvelopeEndpointWithUrlEncodedAuth() : api.getStoreEndpointWithUrlEncodedAuth(),
-  };
-
-  // https://develop.sentry.dev/sdk/envelopes/
-
-  // Since we don't need to manipulate envelopes nor store them, there is no
-  // exported concept of an Envelope with operations including serialization and
-  // deserialization. Instead, we only implement a minimal subset of the spec to
-  // serialize events inline here.
-  if (useEnvelope) {
-    const envelopeHeaders = JSON.stringify({
-      event_id: event.event_id,
-      sent_at: new Date().toISOString(),
-
-      // trace context for dynamic sampling on relay
-      trace: {
-        trace_id: event.contexts?.trace?.trace_id,
-        // TODO: any reason we can't change this property to be called publicKey, since that's what it is?
-        public_key: api.getDsn().user,
-        environment: event.environment || 'no environment specified',
-        release: event.release || 'no release specified',
-      },
-    });
-
-    const itemHeaders = JSON.stringify({
-      type: event.type,
-      // The content-type is assumed to be 'application/json' and not part of
-      // the current spec for transaction items, so we don't bloat the request
-      // body with it.
-      //
-      // content_type: 'application/json',
-      //
-      // The length is optional. It must be the number of bytes in req.Body
-      // encoded as UTF-8. Since the server can figure this out and would
-      // otherwise refuse events that report the length incorrectly, we decided
-      // not to send the length to avoid problems related to reporting the wrong
-      // size and to reduce request body size.
-      //
-      // length: new TextEncoder().encode(req.body).length,
-    });
-
-    // The trailing newline is optional. We intentionally don't send it to avoid
-    // sending unnecessary bytes.
-    //
-    // const envelope = `${envelopeHeaders}\n${itemHeaders}\n${req.body}\n`;
-    const envelope = `${envelopeHeaders}\n${itemHeaders}\n${req.body}`;
-    req.body = envelope;
-  }
-
-  return req;
 }
--- packages/core/test/lib/request.test.ts
+++ packages/core/test/lib/request.test.ts
@@ -30,6 +30,11 @@ describe('eventToSentryRequest', () => {
       event_id: eventId,
       release: 'off.leash.park',
       spans: [],
+      tracestate:
+        'ewAiAGUAbgB2AGkAcgBvAG4AbQBlAG4AdAAiADoAIgBkAG8AZwBwAGEAcgBrACIALAAiAHAAdQBiAGwAaQBjAF8AawBlAHkAIgA6ACIAZAB' +
+        'vAGcAcwBhAHIAZQBiAGEAZABhAHQAawBlAGUAcABpAG4AZwBzAGUAYwByAGUAdABzACIALAAiAHIAZQBsAGUAYQBzAGUAIgA6ACIAbwBmAG' +
+        'YALgBsAGUAYQBzAGgALgBwAGEAcgBrACIALAAiAHQAcgBhAGMAZQBfAGkAZAAiADoAIgAwADkAMAA4ADIAMAAxADMAMAA0ADEANQAyADAAM' +
+        'QAzACIAfQA.',
       transaction: '/dogs/are/great/',
       type: 'transaction',
       user: { id: '1121', username: 'CharlieDog', ip_address: '11.21.20.12' },
@@ -52,12 +57,12 @@ describe('eventToSentryRequest', () => {
     expect(envelope.envelopeHeader).toEqual({
       event_id: eventId,
       sent_at: expect.any(String),
-      trace: {
+      trace: JSON.stringify({
         environment: 'dogpark',
         public_key: 'dogsarebadatkeepingsecrets',
         release: 'off.leash.park',
         trace_id: traceId,
-      },
+      }),
     });
     expect(envelope.itemHeader).toEqual({ type: 'transaction' });
     expect(envelope.event).toEqual(event);
