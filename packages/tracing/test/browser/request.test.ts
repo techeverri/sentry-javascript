ac681cd75c0ca2f79793f80f221091a44eb5f9e7
Katie Byers <lobsterkatie@gmail.com>
2020-12-08 21:15:17 -0800

    add tests for xhr/fetch sentrytrace header

--- packages/tracing/test/browser/request.test.ts
+++ packages/tracing/test/browser/request.test.ts
@@ -63,7 +63,7 @@ describe('callbacks', () => {
   const fetchHandlerData: FetchData = {
     args: ['http://dogs.are.great/', {}],
     fetchData: { url: 'http://dogs.are.great/', method: 'GET' },
-    startTimestamp: 1356996072000,
+    startTimestamp: 2012112120121231,
   };
   const xhrHandlerData: XHRData = {
     xhr: {
@@ -78,18 +78,27 @@ describe('callbacks', () => {
       // setRequestHeader: XMLHttpRequest.prototype.setRequestHeader,
       setRequestHeader,
     },
-    startTimestamp: 1353501072000,
+    startTimestamp: 2012112120121231,
   };
-  const endTimestamp = 1356996072000;
+  const endTimestamp = 2013041520130908;
 
   beforeAll(() => {
-    hub = new Hub(new BrowserClient({ tracesSampleRate: 1 }));
+    hub = new Hub(
+      new BrowserClient({
+        dsn: 'https://dogsarebadatkeepingsecrets@squirrelchasers.ingest.sentry.io/12312012',
+        environment: 'dogpark',
+        release: 'off.leash.park',
+
+        tracesSampleRate: 1,
+      }),
+    );
     makeMain(hub);
   });
 
   beforeEach(() => {
-    transaction = hub.startTransaction({ name: 'organizations/users/:userid', op: 'pageload' }) as Transaction;
+    transaction = hub.startTransaction({ name: 'meetNewDogFriend', op: 'wag.tail' }) as Transaction;
     hub.configureScope(scope => scope.setSpan(transaction));
+    jest.clearAllMocks();
   });
 
   describe('fetchCallback()', () => {
@@ -117,20 +126,17 @@ describe('callbacks', () => {
       expect(spans).toEqual({});
     });
 
-    it('does not add fetch request headers if tracing is disabled', () => {
+    it('does not add tracing headers if tracing is disabled', () => {
       hasTracingEnabled.mockReturnValueOnce(false);
 
       // make a local copy so the global one doesn't get mutated
-      const handlerData: FetchData = {
-        args: ['http://dogs.are.great/', {}],
-        fetchData: { url: 'http://dogs.are.great/', method: 'GET' },
-        startTimestamp: 1353501072000,
-      };
+      const handlerData = { ...fetchHandlerData };
 
       fetchCallback(handlerData, alwaysCreateSpan, {});
 
       const headers = (handlerData.args[1].headers as Record<string, string>) || {};
       expect(headers['sentry-trace']).not.toBeDefined();
+      expect(headers['tracestate']).not.toBeDefined();
     });
 
     it('creates and finishes fetch span on active transaction', () => {
@@ -185,8 +191,15 @@ describe('callbacks', () => {
       expect(newSpan!.status).toBe(SpanStatus.fromHttpCode(404));
     });
 
-    it('adds sentry-trace header to fetch requests', () => {
-      // TODO
+    it('adds tracing headers to fetch requests', () => {
+      // make a local copy so the global one doesn't get mutated
+      const handlerData = { ...fetchHandlerData };
+
+      fetchCallback(handlerData, alwaysCreateSpan, {});
+
+      const headers = (handlerData.args[1].headers as Record<string, string>) || {};
+      expect(headers['sentry-trace']).toBeDefined();
+      expect(headers['tracestate']).toBeDefined();
     });
   });
 
@@ -207,7 +220,7 @@ describe('callbacks', () => {
       expect(spans).toEqual({});
     });
 
-    it('does not add xhr request headers if tracing is disabled', () => {
+    it('does not add tracing headers if tracing is disabled', () => {
       hasTracingEnabled.mockReturnValueOnce(false);
 
       xhrCallback(xhrHandlerData, alwaysCreateSpan, {});
@@ -215,13 +228,14 @@ describe('callbacks', () => {
       expect(setRequestHeader).not.toHaveBeenCalled();
     });
 
-    it('adds sentry-trace header to XHR requests', () => {
+    it('adds tracing headers to XHR requests', () => {
       xhrCallback(xhrHandlerData, alwaysCreateSpan, {});
 
       expect(setRequestHeader).toHaveBeenCalledWith(
         'sentry-trace',
         expect.stringMatching(tracingUtils.TRACEPARENT_REGEXP),
       );
+      expect(setRequestHeader).toHaveBeenCalledWith('tracestate', expect.any(String));
     });
 
     it('creates and finishes XHR span on active transaction', () => {
